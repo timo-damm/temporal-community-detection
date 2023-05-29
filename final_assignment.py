f90807c407f48ab985690e84d64ba373c046293d
Timo Damm <108958755+timo-damm@users.noreply.github.com>
2023-05-29 13:58:05 +0200

    fixing stupid mistake

--- final_assignment.py
+++ final_assignment.py
@@ -19,7 +19,7 @@ def count_distinct(arr):
     distinct = set(arr)
     return len(distinct)
 hist=data[0]
-print(count_distinct(hist))
+#print(count_distinct(hist))
 #interactions = plt.hist(x=hist, bins= 18488, color='#0504aa',
                             #alpha=0.7, rwidth=0.85)
 #plt.xlabel('Day')
@@ -27,7 +27,7 @@ print(count_distinct(hist))
 #plt.title('Interaction Frequencies')
 
 #splitting into days
-print(len(hist))
+#print(len(hist))
 n=np.linspace(1,78249,num=78249)
 seconds=plt.scatter(x=hist, y=n)
 
@@ -39,17 +39,21 @@ sorted(range(len(diff)), key=lambda x: diff[x])
 cuts= sorted(range(len(diff)), key=lambda x: diff[x])[-9:]
 
 #cut in these 9 places to have 10 days data separately
-print(cuts)
-day1 = hist.iloc[0:12161]
-day2 = hist.iloc[12162:20415]
-day3 = hist.iloc[20416:27319]
-day4 = hist.iloc[27320:35774]
-day5 = hist.iloc[35775:43512]
-day6 = hist.iloc[43513:50764]
-day7 = hist.iloc[50765:59893]
-day8 = hist.iloc[59894:64570]
-day9 = hist.iloc[64571:72511]
-day10 = hist.iloc[72511:]
+#print(cuts)
+day1 = data.iloc[0:12161]
+day2 = data.iloc[12162:20415]
+day3 = data.iloc[20416:27319]
+day4 = data.iloc[27320:35774]
+day5 = data.iloc[35775:43512]
+day6 = data.iloc[43513:50764]
+day7 = data.iloc[50765:59893]
+day8 = data.iloc[59894:64570]
+day9 = data.iloc[64571:72511]
+day10 = data.iloc[72511:]
+print(day1)
+
+# making graphs from the separate days
+#G1 = nx.read_edgelist(day1)
 
 # basic model (Louvain for all days, unweighted, no historical continutity)
 
